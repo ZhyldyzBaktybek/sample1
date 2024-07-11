b4b42adfcf4cc4f57c5b4e9d273935fad32ef349
jyldyz <jyldyz.baktybekova@mail.ru>
2024-07-11 15:42:18 +0100

    Added new files

---
+++ tutorial.js
@@ -0,0 +1,29 @@
+//Numeric
+let numOfCats = 9;
+console.log(numOfCats);
+
+//
+let distance = 9.6
+console.log("Distance:" ,distance);
+
+//String
+let name = "Jika";
+console.log("Name is:", name);
+
+//Boolean
+let doorOpen = true;
+console.log(doorOpen);
+
+//Array
+let numbers = [5,6,7,8,9,"cat", true];
+console.log(numbers);
+
+let car = {
+
+    numberOfWheels: 4,
+    modelName: "Model 1",
+    manufacture: 'Tesla'
+``
+}
+console.log(car);
+
